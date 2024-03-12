d626de23e0cf89fb26bcfa2607e6ec1d4050d395
jankni1 <jankni@uni-bremen.de>
2024-03-12 17:03:23 +0100

    fix

--- main.py
+++ main.py
@@ -159,10 +159,11 @@ def index():
         "DE", subdiv="BY", language="en_US", years=2024
     )  # Bayern, da es die meisten Feiertage hat
 
-    meetings_data = cur.execute(
+    cur.execute(
         "SELECT m.m_group, m.date, m.startTime, m.endTime, m.room, m.service, GROUP_CONCAT(u.user_id) as user_ids FROM meetings m JOIN users u ON m.user_id = u.user_id WHERE m.date >= ? GROUP BY m.m_group, m.date, m.startTime, m.endTime, m.room, m.service ORDER BY m.date ASC LIMIT 4",
         (today_date_meetings,),
-    ).fetchall()
+    )
+    meetings_data = cur.fetchall()
 
     cur.execute("SELECT * FROM extras")
     extra_data = cur.fetchall()

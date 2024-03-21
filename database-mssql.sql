933e6b0402e379d4af3844ada46c03f41684f36b
Raphael Schlameuß <raphael.schlameuss@slt-technologies.com>
2024-03-21 11:45:36 +0100

    Add MS SQL database scheme

---
+++ database-mssql.sql
@@ -0,0 +1,192 @@
+USE [SLT_EINSATZPLAN]
+GO
+/****** Object:  Table [dbo].[assignment_table]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[assignment_table](
+	[assignment_id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[car_id] [int] NOT NULL,
+	[project_id] [int] NOT NULL,
+	[startDate] [date] NOT NULL,
+	[endDate] [date] NOT NULL,
+	[week_id] [int] NOT NULL,
+	[year] [int] NOT NULL,
+	[extra1] [nvarchar](255) NOT NULL,
+	[extra2] [nvarchar](255) NOT NULL,
+	[extra3] [nvarchar](255) NOT NULL,
+	[ort] [nvarchar](255) NOT NULL,
+	[group_id] [int] NOT NULL,
+	[hinweis] [nvarchar](255) NOT NULL,
+	[abwesend] [int] NOT NULL,
+	[project_name] [nvarchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[assignment_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[assignment_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[CalendarWeek]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CalendarWeek](
+	[week_id] [int] NOT NULL,
+	[startDate] [date] NOT NULL,
+	[endDate] [date] NOT NULL,
+	[year] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[week_id] ASC,
+	[year] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[cars]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[cars](
+	[car_id] [int] IDENTITY(1,1) NOT NULL,
+	[car_name] [nvarchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[car_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[car_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[customers]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[customers](
+	[customer_id] [int] NOT NULL,
+	[customer_name] [nvarchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[customer_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[extras]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[extras](
+	[id] [nvarchar](255) NOT NULL,
+	[extra_name] [nvarchar](255) NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[group]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[group](
+	[group_id] [int] NOT NULL,
+	[member] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[group_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[meetings]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[meetings](
+	[meeting_id] [int] IDENTITY(1,1) NOT NULL,
+	[user_id] [int] NOT NULL,
+	[date] [date] NOT NULL,
+	[startTime] [time](7) NOT NULL,
+	[endTime] [time](7) NOT NULL,
+	[room] [nvarchar](255) NOT NULL,
+	[service] [int] NOT NULL,
+	[m_group] [int] NOT NULL,
+ CONSTRAINT [PK__meetings__C7B91CAB7A68A9CD] PRIMARY KEY CLUSTERED 
+(
+	[meeting_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[projects]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[projects](
+	[project_id] [int] NOT NULL,
+	[project_name] [nvarchar](255) NOT NULL,
+	[customer_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[project_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[users]    Script Date: 21.03.2024 11:43:08 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[users](
+	[user_id] [int] NOT NULL,
+	[first_name] [nvarchar](255) NOT NULL,
+	[last_name] [nvarchar](255) NOT NULL,
+	[work_field] [nvarchar](255) NOT NULL,
+ CONSTRAINT [PK__users__B9BE370F70208F7C] PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [UQ__users__B9BE370EC98F9DF0] UNIQUE NONCLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[assignment_table]  WITH CHECK ADD FOREIGN KEY([car_id])
+REFERENCES [dbo].[cars] ([car_id])
+GO
+ALTER TABLE [dbo].[assignment_table]  WITH CHECK ADD FOREIGN KEY([group_id])
+REFERENCES [dbo].[group] ([group_id])
+GO
+ALTER TABLE [dbo].[assignment_table]  WITH CHECK ADD FOREIGN KEY([project_id])
+REFERENCES [dbo].[projects] ([project_id])
+GO
+ALTER TABLE [dbo].[assignment_table]  WITH CHECK ADD  CONSTRAINT [FK__assignmen__user___01142BA1] FOREIGN KEY([user_id])
+REFERENCES [dbo].[users] ([user_id])
+GO
+ALTER TABLE [dbo].[assignment_table] CHECK CONSTRAINT [FK__assignmen__user___01142BA1]
+GO
+ALTER TABLE [dbo].[assignment_table]  WITH CHECK ADD FOREIGN KEY([week_id], [year])
+REFERENCES [dbo].[CalendarWeek] ([week_id], [year])
+GO
+ALTER TABLE [dbo].[group]  WITH CHECK ADD  CONSTRAINT [FK__group__member__6E01572D] FOREIGN KEY([member])
+REFERENCES [dbo].[users] ([user_id])
+GO
+ALTER TABLE [dbo].[group] CHECK CONSTRAINT [FK__group__member__6E01572D]
+GO
+ALTER TABLE [dbo].[meetings]  WITH CHECK ADD  CONSTRAINT [FK__meetings__user_i__7A672E12] FOREIGN KEY([user_id])
+REFERENCES [dbo].[users] ([user_id])
+GO
+ALTER TABLE [dbo].[meetings] CHECK CONSTRAINT [FK__meetings__user_i__7A672E12]
+GO
+ALTER TABLE [dbo].[projects]  WITH CHECK ADD FOREIGN KEY([customer_id])
+REFERENCES [dbo].[customers] ([customer_id])
+GO

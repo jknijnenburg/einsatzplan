0de283e607374f772b3f8f39b33cdf8ba3814ee1
Raphael Schlameuß <raphael.schlameuss@gmail.com>
2024-06-24 11:17:50 +0200

    Add Azure deployment environment

---
+++ azure-prebuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#apt update
+#apt install --yes --no-install-recommends tk locales
+#sed -i '/de_DE.UTF-8/s/^# //g' /etc/locale.gen
+#locale-gen
+export LANGUAGE=de_DE:de
+export LANG=de_DE.UTF-8
+export LC_ALL=de_DE.UTF-8
